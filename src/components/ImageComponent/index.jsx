deece3b868599c5c52ab719a4fc9385fe4c50311
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-10 08:44:32 +0300

    Adding Image Component

---
+++ src/components/ImageComponent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageComponent =({ src, alt, className, style })=> {
+    return (
+    <img 
+    src={src}
+    alt={alt}
+    className={className}
+    style={style} />
+    )
+  }
+
+export default ImageComponent;
